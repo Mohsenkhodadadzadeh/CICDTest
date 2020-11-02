ab2775daa0c37217d5576b0a482e740f99d7bf2b
Mohsen khodadadazadeh <mohsen@MacBook-Pro.local>
2020-11-02 21:30:37 +0330

    first commit

---
+++ CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by Mohsen khodadadazadeh on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
